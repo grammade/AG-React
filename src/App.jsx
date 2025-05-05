fcac259bbfd1e5176a279493b1d45d58f8d704b5
norman <normanseiro@gmail.com>
2025-05-05 07:09:54 +0700

    adjust button

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <div className="App-body">
         <GridContext.Provider value={{serverSide, setServerSide}}>
           <Grid/>
-          <button className='button  is-primary m-3' 
+          <button className='button is-info m-3' 
             style={{alignSelf: 'flex-end'}}
             onClick={() => {console.log('test');setServerSide(!serverSide)}}>Set {!serverSide ? 'server' : 'client'} side</button>
         </GridContext.Provider>
