90bdb929503702d1f4ef2c815794767624f159b0
norman <naufal.hisyam@axa-mandiri.co.id>
2025-05-05 17:17:44 +0700

    add infiniteScroll

--- src/Components/Grid.jsx
+++ src/Components/Grid.jsx
@@ -1,103 +1,84 @@
-import { AgGridReact } from 'ag-grid-react'; 
-import { 
+import { AgGridReact } from 'ag-grid-react';
+import {
     useContext,
     useRef,
     useCallback,
-    useState, 
-    useMemo, 
-    useEffect} from 'react';
-import { 
+    useState,
+    useMemo,
+    useEffect
+} from 'react';
+import {
     NumberFilterModule,
     TextFilterModule,
     DateFilterModule,
     QuickFilterModule,
-    AllCommunityModule, 
+    AllCommunityModule,
     ColumnAutoSizeModule,
-    ModuleRegistry } from 'ag-grid-community'; 
+    ModuleRegistry
+} from 'ag-grid-community';
 
-import { ServerSideRowModelModule } from 'ag-grid-enterprise';
 import { GridContext } from '../App';
+import rowData from '../Mock/DataGrid';
 
 import './Grid.css';
 
 
 ModuleRegistry.registerModules([
-    AllCommunityModule, 
+    AllCommunityModule,
     NumberFilterModule,
     TextFilterModule,
     DateFilterModule,
     QuickFilterModule,
-    ServerSideRowModelModule,
     ColumnAutoSizeModule]);
 
-function Grid(){
+function Grid() {
     const gridRef = useRef(null);
-    const {serverSide} = useContext(GridContext);
+    const { serverSide } = useContext(GridContext);
 
-    useEffect(() => {
-        console.log('inGrid: ', serverSide)
-    },[serverSide])
-
-    const [rowData, setRowData] = useState([
-        { BillingNo: "20250504-0001", PolicyNo: "28374619283", BillingDate: "2025-05-08", Product: "MJKFA", MonthsBilled: 3, Amount: 150000.00, Status: "Paid" },
-        { BillingNo: "20250504-0002", PolicyNo: "19283746501", BillingDate: "2025-05-08", Product: "KFC", MonthsBilled: 3, Amount: 50000.00, Status: "Not Paid" },
-        { BillingNo: "20250504-0003", PolicyNo: "37482910476", BillingDate: "2025-05-17", Product: "MKE", MonthsBilled: 6, Amount: 300000.00, Status: "Canceled" },
-        { BillingNo: "20250504-0004", PolicyNo: "91827364500", BillingDate: "2025-05-02", Product: "MCD", MonthsBilled: 2, Amount: 100000.00, Status: "Paid" },
-        { BillingNo: "20250504-0005", PolicyNo: "09182736455", BillingDate: "2025-05-12", Product: "MJKFA", MonthsBilled: 4, Amount: 200000.00, Status: "Not Paid" },
-        { BillingNo: "20250504-0006", PolicyNo: "56473829100", BillingDate: "2025-05-17", Product: "KFC", MonthsBilled: 3, Amount: 150000.00, Status: "Paid" },
-        { BillingNo: "20250504-0007", PolicyNo: "83746519283", BillingDate: "2025-05-22", Product: "MKE", MonthsBilled: 5, Amount: 250000.00, Status: "Canceled" },
-        { BillingNo: "20250504-0008", PolicyNo: "28374619284", BillingDate: "2025-05-12", Product: "MJKFA", MonthsBilled: 3, Amount: 50000.00, Status: "Paid" },
-        { BillingNo: "20250504-0009", PolicyNo: "19283746502", BillingDate: "2025-05-02", Product: "MCD", MonthsBilled: 2, Amount: 100000.00, Status: "Not Paid" },
-        { BillingNo: "20250504-0010", PolicyNo: "37482910477", BillingDate: "2025-05-25", Product: "KFC", MonthsBilled: 6, Amount: 300000.00, Status: "Paid" },
-        { BillingNo: "20250504-0011", PolicyNo: "91827364501", BillingDate: "2025-05-29", Product: "MKE", MonthsBilled: 3, Amount: 150000.00, Status: "Canceled" },
-        { BillingNo: "20250504-0012", PolicyNo: "09182736456", BillingDate: "2025-05-25", Product: "MJKFA", MonthsBilled: 4, Amount: 200000.00, Status: "Not Paid" },
-        { BillingNo: "20250504-0013", PolicyNo: "56473829101", BillingDate: "2025-05-30", Product: "MCD", MonthsBilled: 5, Amount: 250000.00, Status: "Paid" },
-        { BillingNo: "20250504-0014", PolicyNo: "83746519284", BillingDate: "2025-05-17", Product: "MJKFA", MonthsBilled: 2, Amount: 100000.00, Status: "Not Paid" },
-        { BillingNo: "20250504-0015", PolicyNo: "28374619285", BillingDate: "2025-05-22", Product: "KFC", MonthsBilled: 3, Amount: 50000.00, Status: "Paid" },
-        { BillingNo: "20250504-0016", PolicyNo: "19283746503", BillingDate: "2025-05-08", Product: "MKE", MonthsBilled: 3, Amount: 150000.00, Status: "Canceled" },
-        { BillingNo: "20250504-0017", PolicyNo: "37482910478", BillingDate: "2025-05-30", Product: "MCD", MonthsBilled: 6, Amount: 300000.00, Status: "Paid" },
-        { BillingNo: "20250504-0018", PolicyNo: "91827364502", BillingDate: "2025-05-29", Product: "KFC", MonthsBilled: 2, Amount: 100000.00, Status: "Not Paid" },
-        { BillingNo: "20250504-0019", PolicyNo: "09182736457", BillingDate: "2025-05-12", Product: "MJKFA", MonthsBilled: 4, Amount: 200000.00, Status: "Paid" },
-        { BillingNo: "20250504-0020", PolicyNo: "56473829102", BillingDate: "2025-05-22", Product: "MKE", MonthsBilled: 5, Amount: 250000.00, Status: "Canceled" },
-    ]);    
 
     // Column Definitions: Defines the columns to be displayed.
     const [colDefs, setColDefs] = useState([
-      { field: "BillingNo", flex: 2, filter: "agTextColumnFilter" },
-      { field: "PolicyNo", flex: 2, filter: "agTextColumnFilter" },
-      { field: "Product", flex: 1, filter: "agTextColumnFilter" },
-      { field: "BillingDate", flex: 2, filter: "agDateColumnFilter" },
-      {
-        field: "MonthsBilled",
-        headerName: "Months\nBilled",
-        flex: 1,
-        maxWidth: 100,
-        headerClass: "ag-header-whitespace",
-      },
-      {
-        field: "Amount",
-        flex: 2,
-        valueFormatter: (params) => {
-          if (params.value == null) return "";
-          return Number(params.value).toLocaleString("en-US");
+        {
+            field: "BillingNo", flex: 2, filter: "agTextColumnFilter",
+            cellRenderer: (props) => {
+                if (props.value !== undefined) {
+                    return props.value;
+                } else {
+                    return (
+                        <img src="https://www.ag-grid.com/example-assets/loading.gif" />
+                    );
+                }
+            }
         },
-      },
-      {
-        field: "Status",
-        filter: "agTextColumnFilter",
-        flex: 1,
-        maxWidth: 200,
-      },
+        { field: "PolicyNo", flex: 2, filter: "agTextColumnFilter" },
+        { field: "Product", flex: 1, filter: "agTextColumnFilter" },
+        { field: "BillingDate", flex: 2, filter: "agDateColumnFilter" },
+        {
+            field: "MonthsBilled",
+            headerName: "Months\nBilled",
+            flex: 1,
+            maxWidth: 100,
+            headerClass: "ag-header-whitespace",
+        },
+        {
+            field: "Amount",
+            flex: 2,
+            valueFormatter: (params) => {
+                if (params.value == null) return "";
+                return Number(params.value).toLocaleString("en-US");
+            },
+        },
+        {
+            field: "Status",
+            filter: "agTextColumnFilter",
+            flex: 1,
+            maxWidth: 200,
+        },
     ]);
-    const autoSizeStrategy = useMemo(() => {
-        return {
-            type: "fitGridWidth"
-        };
-    }, [])
     const resizeCallback = useCallback(() => {
         gridRef.current.api.sizeColumnsToFit({
             defaultMinWidth: 100
-          });
+        });
     }, [])
     const globalFilterCallback = useCallback(() => {
         gridRef.current.api.setGridOption(
@@ -105,7 +86,7 @@ function Grid(){
             document.getElementById('globalFilter').value
         );
     }, [])
-        
+
     useEffect(() => {
         window.addEventListener("resize", resizeCallback);
         return () => {
@@ -115,8 +96,7 @@ function Grid(){
 
     const datasource = {
         getRows: async (params) => {
-
-            const { startRow, endRow, filterModel, sortModel } = params.request;
+            const { startRow, endRow, filterModel, sortModel } = params;
             const query = {
                 start: startRow,
                 end: endRow,
@@ -124,33 +104,38 @@ function Grid(){
                 sorts: sortModel,
             };
             console.log("[Datasource] Requesting rows", query);
+            console.log("resulting array: ", rowData.slice(startRow, endRow))
 
             await sleep(2000);
-            params.success({rowData: rowData})
+            params.successCallback(rowData.slice(startRow, endRow), rowData.length)
         }
     };
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
+
+
     return (
-        <div className='is-flex is-flex-direction-column is-align-items-center' style={{width:'100%'}}>
+        <div className='is-flex is-flex-direction-column is-align-items-center' style={{ width: '100%' }}>
             <input
-                style={{width: '100%'}}
+                style={{ width: '100%' }}
                 class="input mb-4"
                 type="text"
                 id="globalFilter"
                 placeholder="Search..."
                 onInput={globalFilterCallback}
             />
-            <div style={{ height: 500, width: '100%'}}>
+            <div style={{ height: 200, width: '100%' }}>
                 <AgGridReact
                     key={serverSide ? 'server' : 'client'}
                     ref={gridRef}
-                    rowData={rowData}
+                    {... (!serverSide && { rowData: rowData })}
                     columnDefs={colDefs}
                     {...(serverSide && {
-                        rowModelType:"serverSide",
-                        serverSideDatasource: datasource
+                        rowModelType: "infinite",
+                        datasource: datasource,
+                        cacheBlockSize: 5,
+                        blockLoadDebounceMillis: 2000
                     })}
                 />
             </div>
